5f0cf448efff2186accd0133243fa635b0f49e94
RodrigoSierraV <735@holbertonschool.com>
2019-02-06 23:45:23 +0000

    Fifth script added to the project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * This program 4-puts.c prints exactly "Programming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main (void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
+}	
