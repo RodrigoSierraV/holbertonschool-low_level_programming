0047df95614e3a2225a61465e241b1335e6a2f4f
RodrigoSierraV <735@holbertonschool.com>
2019-02-07 00:15:53 +0000

    sixth script added to the project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - This program 5-printf.c prints exactly
- * with proper grammar, but the outcome is a piece of art
+ * with proper grammar, but the outcome is a piece of art,
  * Return: 0
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art\n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
