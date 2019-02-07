64820538b994d1aef644a9b0900a260fda6f5de1
RodrigoSierraV <735@holbertonschool.com>
2019-02-07 23:28:09 +0000

    Adding program #5, printing single digits base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - prints single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+	for (c = 0; c < 10; c++)
+	{
+	printf("%d", c);
+	}
+	putchar('\n');
+	return (0);
+}
