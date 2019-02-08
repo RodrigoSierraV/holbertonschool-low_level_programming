2b26dfb3ea9d294c1888399c803aaef870dfa67e
RodrigoSierraV <735@holbertonschool.com>
2019-02-08 22:02:05 +0000

    13 program added to the project, prints all possible combinations of two two-digit number

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ *main - prints all possible different combinations of three digits
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+int x;
+int y;
+int z;
+	for (z = 0; z < 10; z = z + 1)
+	{
+	for (c = 0; c < 10; c = c + 1)
+	{
+	for (x = 0; x < 10; x = x + 1)
+	{
+	for (y = 1; y < 10; y = y + 1)
+	{
+	if (y + x > c + z)
+	{
+	putchar(z % 10 + '0');
+	putchar(c % 10 + '0');
+	putchar(' ');
+	putchar(x % 10 + '0');
+	putchar(y % 10 + '0');
+	if (z != 9 || c != 8 || x != 9 || y != 9)
+	{
+	{
+	putchar(44);
+	putchar(' ');
+	}
+	}
+	}
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
