fe7b439e029462b4f499ac345f0805b6abea2903
RodrigoSierraV <735@holbertonschool.com>
2019-02-12 22:01:20 +0000

    Program #7 prints the last digit of a given number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: Given number to extract the last digit
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+int resultado = n % 10;
+return (resultado);
+}
