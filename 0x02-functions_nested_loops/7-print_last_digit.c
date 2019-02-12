c6fabc11896515c3abbd36c1f02a87787b80e0b8
RodrigoSierraV <735@holbertonschool.com>
2019-02-12 23:43:31 +0000

    Program #7, prints and returns the last digit of a number. cutting else if

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,23 +2,21 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @n: Given number to extract the last digit
+ *
+ * description: prints the last digit of a number
  * Return: last digit
  */
 int print_last_digit(int n)
 {
-int resultado;
-if (n > 0)
+int resultado = n % 10;
+if (n < 0)
 {
-resultado = n % 10;
-}
-else if (n < 0)
-{
-resultado = -n % 10;
+_putchar(-resultado + '0');
+ return (-resultado);
 }
 else
 {
-resultado = 0;
-}
 _putchar(resultado + '0');
 return (resultado);
+}
 }
