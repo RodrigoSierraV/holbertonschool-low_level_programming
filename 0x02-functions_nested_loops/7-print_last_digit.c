21a006fae6b87bd20931179f87ee24d63f8bf4f4
RodrigoSierraV <735@holbertonschool.com>
2019-02-12 22:22:23 +0000

    Program #7, prints and returns the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,6 +6,17 @@
  */
 int print_last_digit(int n)
 {
-int resultado = n % 10;
+int resultado;
+if (n >= 0)
+{
+resultado = n % 10;
+_putchar(resultado % 10 + '0');
 return (resultado);
+}
+else
+{
+resultado = -n % 10;
+_putchar(resultado % 10 + '0'); 
+return (resultado);
+}
 }
