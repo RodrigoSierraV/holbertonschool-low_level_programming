5683ea3e7781a8f9605c250306de08f48a30a326
RodrigoSierraV <735@holbertonschool.com>
2019-02-13 19:13:14 +0000

    Program #14, prints first 50 Fibonacci numbers

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 /**
  * main - Prints the first 50 Fibonacci numbers
- * @a: first number of the serire
- * @b: second second number of the serie
  * Return: 0
  */
 int main(void)
 {
-unsigned long int  a = 0;
-unsigned long int  b = 1;
+long int a = 0;
+long int b = 1;
 int c;
+long int add;
 for (c = 0; c < 50; c++)
 {
-unsigned long int add = a + b;
-printf("%lu, ", add);
+add = a + b;
+printf("%li", add);
 a = b;
 b = add;
+if (c < 49)
+{
+printf(", ");
+}
 }
 putchar('\n');
 return (0);
