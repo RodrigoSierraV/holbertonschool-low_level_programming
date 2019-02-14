5fdb4918defe8130a5ea3eef950ed0e8479cb825
RodrigoSierraV <735@holbertonschool.com>
2019-02-14 20:29:20 +0000

    Program #5 prints 10 times the numbers, from 0 to 14

--- 0x03-more_functions_nested_loops/5-more_numbers.c
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,20 @@
 void more_numbers(void)
 {
 int i = 0;
-int b = 
-while (i < 15)
+int b = 0;
+while (i < 10)
 {
-_putchar(i + '0');
+while (b < 15)
+{
+if (b > 9)
+{
+_putchar(1 + '0');
+}
+_putchar(b % 10 + '0');
+b++;
+}
+b = 0;
+_putchar('\n');
 i++;
 }
-_putchar('\n');
 }
