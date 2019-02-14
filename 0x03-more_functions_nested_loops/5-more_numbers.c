7b7119a70aa96aa4537e86dd82d718e16f6b3c68
RodrigoSierraV <735@holbertonschool.com>
2019-02-14 19:04:28 +0000

    Uploading programs 0, 1, 2, 3 and 4

---
+++ 0x03-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ */
+void more_numbers(void)
+{
+int i = 0;
+int b = 
+while (i < 15)
+{
+_putchar(i + '0');
+i++;
+}
+_putchar('\n');
+}
