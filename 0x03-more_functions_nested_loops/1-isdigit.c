50055f9ad80337644a488c1605e0ef8c2e314295
RodrigoSierraV <735@holbertonschool.com>
2019-02-14 15:46:07 +0000

    uploading programs #1, 2 and 3 checked with betty

---
+++ 0x03-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *_isdigit - checks for a digit
+ *@c: integer to test
+ * Return: 1 if digit, otherwise 0
+ */
+int _isdigit(int c)
+{
+if ((c >= 0 && c <= 9) || c == '0')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
