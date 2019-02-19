af2655452d6e6cbf0f2e5bf0ee828425e001a7dd
RodrigoSierraV <735@holbertonschool.com>
2019-02-19 17:53:09 +0000

    Program #4, prints a string, in reverse, followed by a new line.

---
+++ 0x04-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_rev - prints a string, in reverse, followed by a new line.
+ * @s: given string to be printed in reverse
+ *
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+	char b = *s;
+	int count = 0;
+
+	while (b != '\0')
+	{
+		b = *(s + i);
+		count += 1;
+		i++;
+	}
+	count = count - 1;
+	while (count >= 0)
+	{
+		b = *(s + count);
+		_putchar(b);
+		count--;
+	}
+	_putchar('\n');
+}
