b224e0d89befb38053ab2aa5e090269b475c3df4
RodrigoSierraV <735@holbertonschool.com>
2019-02-19 21:26:07 +0000

    program #8,prints n elements of an array of integers, followed by a new line. Correction with betty style

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * print_array - prints n elements of an array of integers, followed by a new line
+ * print_array - prints n elements of an array of integers,
+ * followed by a new line
  * @a: given array
  * @n: number of elements to be printed
  */
 void print_array(int *a, int n)
 {
 	int i = 0;
-	
+
 	while (i < n)
 	{
-		printf("%d",*(a + i));
+		printf("%d", *(a + i));
 		i++;
 		if (i < n)
 		printf(", ");
