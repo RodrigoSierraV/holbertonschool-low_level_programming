ba315e786444c0e092db243f6aa5cc9b86b52f53
RodrigoSierraV <735@holbertonschool.com>
2019-02-21 07:19:30 +0000

    Program #10, Correction of the sign of the integer

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -12,10 +12,17 @@ int _atoi(char *s)
 	int b = 0;
 	int d = 0;
 	unsigned int c = 0;
-	int start = 0;
+	int signo = 1;
+	int signofinal = 1;
 
 	while (*(s + i) != '\0')
 	{
+		if (s[i] == 45)
+		{
+			signo = signo * (-1);
+			if ((s[i + 1] - '0') >= 0 && (s[i + 1] - '0') <= 9)
+				signofinal = signo;
+		}
 		i++;
 	}
 	count = i - 1;
@@ -25,9 +32,6 @@ int _atoi(char *s)
 		b = *(s + i) - '0';
 		if (b >= 0 && b <= 9)
 		{
-			d++;
-			if (d == 1)
-				start = i;
 			c = (c * 10) + b;
 			d = i + 1;
 			if ((*(s + d) - '0') < 0 || (*(s + d) - '0') > 9)
@@ -35,12 +39,5 @@ int _atoi(char *s)
 		}
 		i++;
 	}
-	if (s[start - 1] == 45)
-	{
-		return (-c);
-	}
-	else
-	{
-		return (c);
-	}
+	return (signofinal * c);
 }
