c50fcab174620511cae46d3bf7eb6bf43d188f25
RodrigoSierraV <735@holbertonschool.com>
2019-02-21 23:40:54 +0000

    Program #2, correction with one variable less

--- 0x05-pointers_arrays_strings/2-strncpy.c
+++ 0x05-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * *_strncpy / copies the string pointed to by src,
+ * *_strncpy - copies the string pointed to by src,
  * @src: given string to be copied
  * @dest: buffer to create the copy of src
  * @n: size of src
@@ -10,16 +10,17 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
-	int destlen = 0;
-
+	
 	for (i = 0; i < n; i++)
 	{
 		if (*(src + i) != '\0')
 		{
-		*(dest + i) = *(src + i);
+			*(dest + i) = *(src + i);
 		}
 	}
 	for ( ; i < n; i++)
+	{
 		*(dest + i) = '\0';
+	}
 	return (dest);
 }
