54219f09724f5a8d75590610efe577c12b4fec38
RodrigoSierraV <735@holbertonschool.com>
2019-02-21 04:22:10 +0000

    Program #10, with the correction of numbers to return

--- 0x04-pointers_arrays_strings/100-atoi.c
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,7 @@ int _atoi(char *s)
 	int i = 0;
 	int count = 0;
 	int b = 0;
-	int d = 0;	
+	int d = 0;
 	int c = 0;
 
 	while (*(s + i) != '\0')
@@ -24,23 +24,21 @@ int _atoi(char *s)
 		b = *(s + i) - '0';
 		if (b >= 0 && b <= 9)
 		{
-			c = (c *10) + b;
+			c = (c * 10) + b;
 			d = i + 1;
 			if ((*(s + d) - '0') >= 0 && (*(s + d) - '0') <= 9)
 			{
-				
 			}
 			else
 			{
-                        	i = count;
+				i = count;
 			}
 			i++;
-			
 		}
 		else
 		{
 			i++;
 		}
 	}
-	return(c);
+	return (c);
 }
