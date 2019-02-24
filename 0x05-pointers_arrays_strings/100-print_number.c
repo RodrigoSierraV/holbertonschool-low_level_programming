991b5b64a18c63c8e2d3f96bb24373dfe7650b97
RodrigoSierraV <735@holbertonschool.com>
2019-02-24 21:51:58 +0000

    Function #9, prints an integer only with _putchar. With limits.h library

--- 0x05-pointers_arrays_strings/100-print_number.c
+++ 0x05-pointers_arrays_strings/100-print_number.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include <limits.h>
 /**
 * print_number - prints an integer with _putchar.
 * @n: given integer to print
@@ -15,6 +16,10 @@ int residuo, z;
 int div;
 int digit;
 
+	if (n > INT_MAX)
+		n = 2147483647;
+	if (n < INT_MIN)
+		n = -2147483648;
 	while (digitcount != 0)
 	{
 		digitcount = (digitcount - (digitcount % 10)) / 10;
