aefabbf47bc6e9667fd9383cc2f2071ad5df8a38
RodrigoSierraV <735@holbertonschool.com>
2019-02-26 21:49:44 +0000

    Function #3, gets the length of a prefix substring, the same as strspn function in C

--- 0x06-pointers_arrays_strings/3-strspn.c
+++ 0x06-pointers_arrays_strings/3-strspn.c
@@ -1,30 +1,38 @@
 #include "holberton.h"
+#include <stdio.h>
 /**
- * _strspn - gets the length of a prefix substring.
- * @s: segment to assess
- * @accept: bytes to search
+ * *_strspn -  gets the length of a prefix substring.
+ * @s: string to scan
+ * @accept: substring to search in s
  *
- * Return: number of bytes in the initial segment of s
+ * Return: the number of bytes in the initial segment of s
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int len = 0;
-	int b = 0;
 	int i = 0;
-	unsigned int num = 0;
+	int z = 0;
+	int lenh = 0;
+	int lend = 0;
+	int sum = 0;
 
-	while (*(s + len) != '\0')
-		len++;
-	while (*(s + b) != ',')
-	{
-		while (*(accept + i) != '\0')
+	while (*(s + lenh) != '\0')
+		lenh++;
+	while (*(accept + lend) != '\0')
+		lend++;
+		while (i < lenh)
 		{
-			if (*(s + b) == *(accept + i))
-				num += 1;
+			z = 0;
+			while (z < lend)
+			{
+				if (*(s + i) == *(accept + z))
+				{
+					sum += 1;
+				}
+				z++;
+			}
+			if (sum < i)
+			  break;
 			i++;
 		}
-		b++;
-		i = 0;
-	}
-	return (num);
+		return (sum);
 }
