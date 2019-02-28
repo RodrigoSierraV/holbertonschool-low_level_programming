b1d934c8a9829fb17b07e478bd81563c37447fc7
RodrigoSierraV <735@holbertonschool.com>
2019-02-28 04:01:10 +0000

    Function #1, prints a string in reverse.

--- 0x07-recursion/1-print_rev_recursion.c
+++ 0x07-recursion/1-print_rev_recursion.c
@@ -1,17 +1,43 @@
 #include "holberton.h"
 /**
- * _print_rev_recursion - prints a string in reverse.
- * @s: pointed string to print
- *
+ * endpointer - finds length of s
+ * @s: pointed string to measure
+ * @len: counter of characters in s
+ * Return: number of characters in s
  */
-void _print_rev_recursion(char *s)
+int endpointer(char *s, int len)
 {
 	if (*s == '\0')
 	{
-		_putchar('\n');
-		return;
+		return (len);
 	}
-	_putchar(*s);
 	s++;
-	_puts_recursion(s);
+	len++;
+	return (endpointer(s, len));
+}
+/**
+ * printchar - prints string s in reverse
+ * @s: pointed string to print
+ * @len: counter of characters in s
+ *
+ */
+void printchar(char *s, int len)
+{
+	if (len < 0)
+		return;
+	_putchar(*(s + len));
+	len--;
+	printchar(s, len);
+}
+/**
+ * _print_rev_recursion - prints a string in reverse.
+ * @s: pointed string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	int len = 0;
+	int length = 0;
+
+	length = endpointer(s, len);
+	printchar(s, length);
 }
