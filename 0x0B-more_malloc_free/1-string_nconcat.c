f1b9f30b4b1be793c14408176697aa704d0adc1d
RodrigoSierraV <735@holbertonschool.com>
2019-03-07 21:45:41 +0000

    Function #1, concatenates two strings.

---
+++ 0x0B-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates two strings.
+ *
+ * @s1: passed string to concatenate
+ * @s2: passed string to concatenate
+ * @n: first bytes of s2 to concatenate
+ * Return: a pointer to a newly allocated space in memory
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *copy, len1 = '0', len2 = '0';
+	unsigned int i = 0, j = 0, k = 0, l = 0;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+		i = 1;
+	}
+	for (i = 0; len1 != '\0'; i++)
+		len1 = *(s1 + i);
+	if (s2 == NULL)
+	{
+		s2 = "";
+		j = 0;
+	}
+	for (j = 0; len2 != '\0'; j++)
+		len2 = *(s2 + j);
+	copy = malloc((i + 1 + n) * sizeof(char));
+	if (copy == NULL)
+		return (NULL);
+	while (k < (i - 1))
+	{
+		copy[k] = s1[k];
+		k++;
+	}
+	while (k < ((i - 1) + n))
+	{
+		copy[k] = s2[l];
+		k++;
+		if (k == ((i - 1) + n))
+			copy[k] = '\0';
+		l++;
+	}
+	return (copy);
+}
