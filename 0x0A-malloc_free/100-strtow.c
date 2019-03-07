8cf46997dd8151ccd8160b472c53314540af7244
RodrigoSierraV <735@holbertonschool.com>
2019-03-07 06:00:53 +0000

    Function #6, splits a string into words.

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -2,22 +2,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * print_tab - Prints an array of string
- * @tab: The array to print
+ * strtow -  splits a string into words.
+ * @str: given string
  *
- * Return: nothing
+ * Return: pointer to an array of strings
  */
 char **strtow(char *str)
 {
-  int len = 0, i = 0;
-  char **aux;
-  
-  while (str[len])
-      len++;
-  printf("%d", len);
-  aux = malloc(len);
-  while (i < len)
-    {
-    aux[i] = str[i];
-  return (aux);
+	char **aux;
+	int i = 0, j = 0, len = 0, letters = 0, words = 0, k = 0;
+
+	while (str[len])
+	{
+		if (str[len] != 32 && str[len + 1] == 32)
+		words++;
+		len++;
+	}
+	aux = (char **)malloc((words + 1) * sizeof(char *));
+	len = 0;
+	while (str[len])
+	{
+		if (str[len] != 32)
+			letters++;
+		if (str[len] != 32 && str[len + 1] == 32)
+		{
+			aux[i] = (char *)malloc((letters + 1) * sizeof(char));
+			letters = 0;
+			i++;
+		}
+		len++;
+	}
+	i = 0;
+	while (str[k])
+	{
+		if (str[k] != 32)
+		{
+			*(*(aux + i) + j) = str[k];
+			j++;
+		}
+		if (str[k] != 32 && str[k + 1] == 32)
+		{
+			*(*(aux + i) + (j + 1)) = '\0';
+			i++;
+			j = 0;
+		}
+		k++;
+	}
+	return (aux);
 }
