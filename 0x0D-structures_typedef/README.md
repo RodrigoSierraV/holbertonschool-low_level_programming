7b28cbccd485c024d851d83a60b87efd3bf858d4
RodrigoSierraV <735@holbertonschool.com>
2019-03-11 16:12:29 +0000

    Task #0, Define a new type struct dog

---
+++ 0x0D-structures_typedef/README.md
@@ -0,0 +1,3 @@
+This project is about the construction of structures in C, and how to use them. Another objective is the use of the keyword typedef to give new names to certain types of variables.
+
+Each commit will have the purpose of each program to explain its functionality.
---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - characteristics of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: name of the owner
+ *
+ * Description: this structure put together some characteristics of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
