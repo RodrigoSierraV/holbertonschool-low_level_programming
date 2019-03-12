5543d8075707cd37e77892afc9ae419f9325ca85
RodrigoSierraV <735@holbertonschool.com>
2019-03-12 01:26:31 +0000

    Function #4, creates a new dog. With Betty style

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -3,12 +3,12 @@
 #include "dog.h"
 
 /**
- * init_dog - initialize a variable of type struct dog
+ * new_dog - creates a new dog.
  *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: name of the owner of the dog
- * Return: 
+ * Return: pointer to newdog, type dog_t
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -17,7 +17,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
 		return (NULL);
-	newdog->name = name; 
+	newdog->name = name;
 	newdog->age = age;
 	newdog->owner = owner;
 	return (newdog);
