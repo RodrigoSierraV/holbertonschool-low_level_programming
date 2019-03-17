01d84b301c4ae598ec62ea9922d29338cb7b01e4
RodrigoSierraV <735@holbertonschool.com>
2019-03-17 01:38:07 +0000

    Function #1, prints numbers, followed by a new line. Without validation of n

--- 0x0F-variadic_functions/1-print_numbers.c
+++ 0x0F-variadic_functions/1-print_numbers.c
@@ -12,8 +12,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list arguments;
 	unsigned int i = 0;
 
-	if (n == 0)
-		return;
 	va_start(arguments, n);
 	while (i < n)
 	{
