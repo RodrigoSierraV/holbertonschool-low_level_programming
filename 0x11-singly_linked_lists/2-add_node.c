7eb8427b5be35925c3346f7baf9b2e70130291c2
RodrigoSierraV <735@holbertonschool.com>
2019-03-21 23:22:24 +0000

    Function #2, adds a new node at the beginning of a list_t list. With free if fails

--- 0x11-singly_linked_lists/2-add_node.c
+++ 0x11-singly_linked_lists/2-add_node.c
@@ -17,6 +17,8 @@ list_t *add_node(list_t **head, const char *str)
 
 	newnode = malloc(sizeof(list_t));
 	if (newnode == NULL)
+	{
+		free(newnode);
 		return (NULL);
 	newnode->str = dup;
 	while (dup[len])
