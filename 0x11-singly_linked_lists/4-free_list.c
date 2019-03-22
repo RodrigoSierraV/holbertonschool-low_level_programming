938bc6abd11c7f9c8fcecc4f07debb53a724fe86
RodrigoSierraV <735@holbertonschool.com>
2019-03-22 04:29:46 +0000

    Function #4, frees a list_t list.

---
+++ 0x11-singly_linked_lists/4-free_list.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: given linked list
+ * Return: nothing.
+ */
+void free_list(list_t *head)
+{
+	if (head->next != NULL)
+		free_list(head->next);
+	free(head);
+}
