c8227c0030d551e0fa1dbbe0dab333155d618112
RodrigoSierraV <735@holbertonschool.com>
2019-03-26 05:12:52 +0000

    Correction of requirement in function #0

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -13,8 +13,6 @@ size_t print_listint(const listint_t *h)
 	listint_t *current = (listint_t *)h;
 	int nodes = 0;
 
-	if (h == NULL)
-		return (0);
 	while (current)
 	{
 		printf("%d\n", current->n);
