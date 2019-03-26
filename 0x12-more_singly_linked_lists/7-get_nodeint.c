ed551b0aa95bdeca53543b00b11d11cd862ff09c
RodrigoSierraV <735@holbertonschool.com>
2019-03-26 15:20:18 +0000

    Function #7, returns the nth node of a listint_t linked list. With conditional if requested node doesn't exist

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -19,6 +19,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	while (element != index)
 	{
 		element++;
+		if (current->next == NULL)
+			return (NULL);
 		current = current->next;
 	}
 	return (current);
