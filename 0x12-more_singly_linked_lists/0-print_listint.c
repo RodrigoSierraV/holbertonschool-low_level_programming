f10a611e55a4b7549689b8f1a6c108e8b63a340d
RodrigoSierraV <735@holbertonschool.com>
2019-03-26 22:09:03 +0000

    Function #0, removing conditional when h = NULL

--- 0x12-more_singly_linked_lists/0-print_listint.c
+++ 0x12-more_singly_linked_lists/0-print_listint.c
@@ -10,17 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	if (h == NULL)
-	{
-		return (0);
-	}
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		nodes++;
 		h = h->next;
 	}
-	printf("%d\n", h->n);
-	nodes++;
 	return (nodes);
 }
