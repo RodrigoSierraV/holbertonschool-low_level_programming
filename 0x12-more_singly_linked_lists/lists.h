527bf8c2880ae71f755ce8902ed963ff323dfe36
RodrigoSierraV <735@holbertonschool.com>
2019-03-27 04:27:54 +0000

    Header file lists.h with betty

--- 0x12-more_singly_linked_lists/lists.h
+++ 0x12-more_singly_linked_lists/lists.h
@@ -21,7 +21,7 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-void startupfun (void) __attribute__ ((constructor));
+void startupfun(void) __attribute__ ((constructor));
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -32,8 +32,8 @@ void startupfun (void) __attribute__ ((constructor));
  */
 typedef struct listint_s
 {
-  int n;
-  struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
