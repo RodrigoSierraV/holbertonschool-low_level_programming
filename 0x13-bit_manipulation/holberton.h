b303d917caccfa77ee083c8a79cb8ce9f91f5e40
RodrigoSierraV <735@holbertonschool.com>
2019-03-28 21:16:34 +0000

    Function #0, converts a binary number to an unsigned int. Betty in holberton.h

--- 0x13-bit_manipulation/holberton.h
+++ 0x13-bit_manipulation/holberton.h
@@ -3,7 +3,8 @@
 
 /**
  * _putchar - writes the character c to stdout
- *
+ * @c: given character to print
+ * Return: ascii number.
  */
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
