c0b33d6c188d86fab0d456d85bf3b26fbae18c45
RodrigoSierraV <735@holbertonschool.com>
2019-03-28 21:12:31 +0000

    Function #0, converts a binary number to an unsigned int.

---
+++ 0x13-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+This project is about how to manipulate bits and use bitwise operators. Every function will have described its purpose in every commit. 
---
+++ 0x13-bit_manipulation/holberton.h
@@ -0,0 +1,10 @@
+#ifndef HOLBERTON_H_
+#define HOLBERTON_H_
+
+/**
+ * _putchar - writes the character c to stdout
+ *
+ */
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+#endif /*HOLBERTON_H_ */
