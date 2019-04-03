15b57f7bc4f6e3ddce5d1f136f14bc14b367425e
RodrigoSierraV <735@holbertonschool.com>
2019-04-03 01:20:14 +0000

    Function #2,  appends text at the end of a file.

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -16,4 +16,5 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 #endif /*HOLBERTON_H_ */
