2d5bb5001ca416524f4cf6bcbe7f587956365e21
RodrigoSierraV <735@holbertonschool.com>
2019-04-03 02:56:21 +0000

    Function #4,  copies the content of a file to another file.

---
+++ 0x14-file_io/3-cp.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <errno.h>
+/**
+ * main -  copies the content of a file to another file.
+ * @argc: number of arguments from command-line
+ * @argv: pointer to arguments from command-line
+ * Return: always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int tocreate, topen, towrite, toread, toclose;
+	char buf[1025];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	if (!argv[1])
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file1 %s\n", argv[1]);
+		exit(98);
+	}
+	tocreate = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (tocreate == -1)
+	{		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	topen = open(argv[1], O_RDONLY);
+	if (topen == -1)
+	{		dprintf(STDERR_FILENO, "Error: Can't read from file2 %s\n", argv[1]);
+		exit(98);
+	}
+	toread = read(topen, buf, 1024);
+	if (toread == -1)
+	{		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	towrite = write(tocreate, buf, toread);
+	if (towrite == -1 || towrite != toread)
+	{		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	toclose = close(tocreate);
+	if (toclose == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tocreate);
+		exit(100);
+	}
+	return (0);
+}
