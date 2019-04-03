697693d8ebe71f726ef7c4cfde47b0645ef2946f
RodrigoSierraV <735@holbertonschool.com>
2019-04-03 03:24:28 +0000

    Function #3, copies the content of a file to another file. Adding loop if file from > 1024 bytes

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -14,17 +14,15 @@
  */
 int main(int argc, char *argv[])
 {
-	int tocreate, topen, towrite, toread, toclose;
+	int tocreate, topen, towrite = 1024, toread, toclose;
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	{		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	if (!argv[1])
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+	{		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	tocreate = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
@@ -37,20 +35,22 @@ int main(int argc, char *argv[])
 	{		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	toread = read(topen, buf, 1024);
-	if (toread == -1)
-	{		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	towrite = write(tocreate, buf, toread);
-	if (towrite == -1 || towrite != toread)
-	{		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+	while (towrite == 1024)
+	{
+		toread = read(topen, buf, 1024);
+		if (toread == -1)
+		{		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		towrite = write(tocreate, buf, toread);
+		if (towrite == -1 || towrite != toread)
+		{	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
 	}
 	toclose = close(tocreate);
 	if (toclose == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tocreate);
+	{		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", tocreate);
 		exit(100);
 	}
 	return (0);
