77411da8d6d227a2f30a51e164e6f66200c99424
RodrigoSierraV <735@holbertonschool.com>
2019-04-03 00:08:24 +0000

    Function #1, creates a file.

--- 0x14-file_io/holberton.h
+++ 0x14-file_io/holberton.h
@@ -15,4 +15,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int get_endianness(void);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 #endif /*HOLBERTON_H_ */
