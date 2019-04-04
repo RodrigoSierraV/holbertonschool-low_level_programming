6f78c6a1f550d745bc6ec1c483ba5f483b62d477
RodrigoSierraV <735@holbertonschool.com>
2019-04-04 02:36:32 +0000

    Function #16, finds and prints the first 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,25 +1,47 @@
+
+
+
 #include <stdio.h>
 /**
- * main - Prints the first 50 Fibonacci numbers
+ * main - Prints the first 98 Fibonacci numbers
  * Return: 0
  */
 int main(void)
 {
-unsigned long a = 0;
-unsigned long b = 1;
-int c;
-unsigned long add;
-for (c = 0; c < 98; c++)
-{
-add = a + b;
-printf("%llu", add);
-a = b;
-b = add;
-if (c < 98)
-{
-printf(", ");
-}
-}
-putchar('\n');
-return (0);
+	unsigned long a = 0, resa = 0, resb = 0;
+	unsigned long b = 1;
+	int c;
+	unsigned long add = 0, shortadd = 0;
+
+	for (c = 0; c < 98; c++)
+	{
+		if (c >= 92)
+		{
+			if (c == 92)
+			{
+				resa = a % 1000;
+				resb = b % 1000;
+				a = a / 1000;
+				b = b / 1000;
+			}
+			shortadd = resa + resb;
+			add = a + b + (shortadd / 1000);
+			printf("%lu%03lu", add, shortadd % 1000);
+			resa = resb;
+			a = b;
+			resb = shortadd % 1000;
+			b = add;
+		}
+		else
+		{
+			add = a + b;
+			printf("%lu", add);
+			a = b;
+			b = add;
+		}
+		if (c < 97)
+			printf(", ");
+	}
+	putchar('\n');
+	return (0);
 }
