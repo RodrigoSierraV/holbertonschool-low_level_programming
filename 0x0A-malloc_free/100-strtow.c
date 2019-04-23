d384aa513bc5c7735bf7898cf2f4be866b871b75
RodrigoSierraV <735@holbertonschool.com>
2019-04-23 05:55:03 +0000

    adding betty style with separated functions

--- 0x0A-malloc_free/100-strtow.c
+++ 0x0A-malloc_free/100-strtow.c
@@ -1,6 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 #include <stdlib.h>
+char **malloc_ed(char *str, int words);
 /**
  * strtow -  splits a string into words.
  * @str: given string
@@ -10,18 +11,53 @@
 char **strtow(char *str)
 {
 	char **aux;
-	int i = 0, j = 0, l = 0, letters = 0, words = 0, k = 0;
+	int i = 0, j = 0, l = 0, words = 0, k = 0;
 
+	if (str == NULL)
+		return (NULL);
 	while (str[l])
 	{
 		if (str[l] != 32 && str[l + 1] == 32)
 		words++;
 		l++;
 	}
+	if (words == 0)
+		return (NULL);
+	aux = malloc_ed(str, words);
+	if (aux == NULL)
+		return (NULL);
+	while (str[k])
+	{
+		if (str[k] != 32)
+		{
+			*(*(aux + i) + j) = str[k];
+			j++;
+		}
+		if (str[k] != 32 && (str[k + 1] == 32 || str[k + 1] == '\0'))
+		{
+			*(*(aux + i) + (j + 1)) = '\0';
+			i++;
+			j = 0;
+		}
+		k++;
+	}
+	*(aux + i) = NULL;
+	return (aux);
+}
+/**
+ * malloc_ed - allocates space in memory.
+ * @str: given string
+ * @words: amount of words in str
+ * Return: pointer to an array of arrays
+ */
+char **malloc_ed(char *str, int words)
+{
+	char **aux = NULL;
+	int l = 0, letters = 0, i = 0;
+
 	aux = (char **)malloc((words + 1) * sizeof(char *));
 	if (aux == NULL)
 		return (NULL);
-	l = 0;
 	while (str[l])
 	{
 		if (str[l] != 32)
@@ -44,22 +80,5 @@ char **strtow(char *str)
 		}
 		l++;
 	}
-	i = 0;
-	while (str[k])
-	{
-		if (str[k] != 32)
-		{
-			*(*(aux + i) + j) = str[k];
-			j++;
-		}
-		if (str[k] != 32 && (str[k + 1] == 32 || str[k + 1] == '\0'))
-		{
-			*(*(aux + i) + (j + 1)) = '\0';
-			i++;
-			j = 0;
-		}
-		k++;
-	}
-	*(aux + i) = NULL;
 	return (aux);
 }
