283e644f85c463761ee5f430391c44511fa64890
RodrigoSierraV <735@holbertonschool.com>
2019-05-05 00:45:16 +0000

    the largest palindrome made from the product of two 3-digit numbers.

--- 0x16-doubly_linked_lists/palind.c
+++ 0x16-doubly_linked_lists/palind.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 void main(void)
 {
-	int a = 100, b, number = 0, i = 0, pal = 0, test = 0;
+	int a = 0, b, number = 0, i = 0, pal = 0, test = 0;
 	int store[6], comp = 0, palindrome = 0, z = 0;
 
 	while (a < 1000)
 	{
-		b = 100;
+		b = 0;
 		while (b < 1000)
 		{
 			number = a * b;
@@ -33,7 +33,6 @@ void main(void)
 			if (comp == i / 2)
 				palindrome = number;
 			b++;
-			/**printf("n:%d pal:%d a:%d b:%d\n", number, palindrome, a, b);*/
 		}
 		a++;
 	}
