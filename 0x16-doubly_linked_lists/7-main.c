9be7b54c2dc880abb59023a700ffe98004be3dc6
RodrigoSierraV <735@holbertonschool.com>
2019-05-06 21:10:35 +0000

    Fixing when adding a node at the end of the list

--- 0x16-doubly_linked_lists/7-main.c
+++ 0x16-doubly_linked_lists/7-main.c
@@ -13,17 +13,17 @@ int main(void)
   dlistint_t *head;
 
   head = NULL;
-  /**add_dnodeint_end(&head, 0);
+  add_dnodeint_end(&head, 0);
   add_dnodeint_end(&head, 1);
   add_dnodeint_end(&head, 2);
   add_dnodeint_end(&head, 3);
-  add_dnodeint_end(&head, 4);*/
+  add_dnodeint_end(&head, 4);
   add_dnodeint_end(&head, 2);
   add_dnodeint_end(&head, -12);
   add_dnodeint_end(&head, 6);
   print_dlistint(head);
   printf("-----------------\n");
-  insert_dnodeint_at_index(&head, 3, 150);
+  insert_dnodeint_at_index(&head, 0, 150);
   print_dlistint(head);
   free_dlistint(head);
   head = NULL;
