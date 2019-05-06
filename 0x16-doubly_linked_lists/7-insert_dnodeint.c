c7a1604f0e4de5cb03591d3ab20017abd07f53cb
RodrigoSierraV <735@holbertonschool.com>
2019-05-06 22:07:54 +0000

    Solving problems when adding a node at the end

--- 0x16-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x16-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,8 +33,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		aux = aux->next;
 		i++;
 	}
-	if (idx == len - 1)
+	/**
+	* if (idx == len - 1)
 		return (add_dnodeint_end(h, n));
+	 */
 	if (aux == NULL)
 		return (NULL);
 	new->next = aux;
