c12148d3e36c4c1dbf4e51f6c46c1de09c4516c4
RodrigoSierraV <735@holbertonschool.com>
2019-06-01 03:44:46 +0000

    Task #3, function that adds an element to the hash table

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -23,7 +23,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = strdup(value);
 	if (!new->value)
 		return (0);
-	new->key = strdup(key)
+	new->key = strdup(key);
 	if (!new->key)
 		return (0);
 	new->next = NULL;
