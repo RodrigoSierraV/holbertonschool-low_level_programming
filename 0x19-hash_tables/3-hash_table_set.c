fd74cd059ca35b86bc9a7065366b6a64e3931021
RodrigoSierraV <735@holbertonschool.com>
2019-06-01 04:25:03 +0000

    Fixin when key already in linked list

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -11,28 +11,46 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int keynumber;
-	hash_node_t *new;
+	hash_node_t *new = NULL, *aux = NULL;
 
 	if (!ht || !ht->array || !key || strlen(key) == 0 || !value ||
 		ht->size == 0)
 		return (0);
 	keynumber = key_index((unsigned char *)key, ht->size);
-	new = (hash_node_t *)malloc(sizeof(hash_node_t));
-	if (!new)
-		return (0);
-	new->value = strdup(value);
-	if (!new->value)
-		return (0);
-	new->key = strdup(key);
-	if (!new->key)
-		return (0);
-	new->next = NULL;
-	if (!ht->array[keynumber])
-		ht->array[keynumber] = new;
+
+	aux = ht->array[keynumber];
+	while (aux)
+	{
+		if (strmp(aux->key, key) == 0)
+			break;
+		aux = aux->next	;
+	}
+	if (aux)
+	{
+		free(aux->value);
+		aux->value = strdup(value);
+		if (!aux->value)
+			return (0);
+	}
 	else
 	{
-		new->next = ht->array[keynumber]->next;
-		ht->array[keynumber] = new;
+		new = (hash_node_t *)malloc(sizeof(hash_node_t));
+		if (!new)
+			return (0);
+		new->value = strdup(value);
+		if (!new->value)
+			return (0);
+		new->key = strdup(key);
+		if (!new->key)
+			return (0);
+		new->next = NULL;
+		if (!ht->array[keynumber])
+			ht->array[keynumber] = new;
+		else
+		{
+			new->next = ht->array[keynumber]->next;
+			ht->array[keynumber] = new;
+		}
 	}
 	return (1);
 }
