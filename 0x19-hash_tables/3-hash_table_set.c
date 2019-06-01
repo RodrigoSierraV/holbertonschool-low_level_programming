9ece776883a12fa2cd3aa7cf57fa32ae214b9793
RodrigoSierraV <735@holbertonschool.com>
2019-06-01 04:29:00 +0000

    Fixin when key already in linked list

--- 0x19-hash_tables/3-hash_table_set.c
+++ 0x19-hash_tables/3-hash_table_set.c
@@ -20,7 +20,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	aux = ht->array[keynumber];
 	while (aux)
 	{
-		if (strmp(aux->key, key) == 0)
+		if (strcmp(aux->key, key) == 0)
 			break;
 		aux = aux->next;
 	}
