6188ab6e7f0c51ad4a8e6ef008e55cb480049bf4
RodrigoSierraV <735@holbertonschool.com>
2019-07-24 20:34:37 +0000

    Redefining conditionals

--- 0x1D-binary_trees/17-binary_tree_sibling.c
+++ 0x1D-binary_trees/17-binary_tree_sibling.c
@@ -12,18 +12,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 
-	if (node->n == node->parent->right->n)
-	{
-		if (!node->parent->left)
-			return (NULL);
-		else
-			return (node->parent->left);
-	}
+	if (node->parent->right == node)
+		return (node->parent->left);
 	else
-	{
-		if (!node->parent->right)
-			return (NULL);
-		else
-			return (node->parent->right);
-	}
+		return (node->parent->right);
 }
